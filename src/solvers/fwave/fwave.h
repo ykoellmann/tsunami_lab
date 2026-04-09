633fc5f156c7cef21b6628b6a2ad7e4aeca4c625
mbbrueckner <mikabenbrueckner@icloud.com>
2026-04-09 12:58:03 +0200

    feat: add F-wave solver header file, according to task 1.3

--- src/solvers/fwave/fwave.h
+++ src/solvers/fwave/fwave.h
@@ -1,16 +1,100 @@
-//
-// Created by Yannik Köllmann on 09.04.26.
-//
+/**
+ * @author Yannik Köllmann (yannik.koellmann AT uni-jena.de)
+ * @author Jan Vogt (jan.vogt AT uni-jena.de)
+ * @author Mika Brückner (mika.brueckner AT uni-jena.de)
+ * @section DESCRIPTION
+ * F-wave solver for the shallow water equations.
+ **/
 
-#ifndef TSUNAMI_LAB_FWAVE_H
-#define TSUNAMI_LAB_FWAVE_H
+#ifndef TSUNAMI_LAB_SOLVERS_FWAVE
+#define TSUNAMI_LAB_SOLVERS_FWAVE
+
+#include "../../constants.h"
+
+namespace tsunami_lab
+{
+   namespace solvers
+   {
+      class FWave;
+   }
+} // namespace tsunami_lab
+
+class tsunami_lab::solvers::FWave
+{
+private:
+   //! gravity constant
+   static t_real constexpr m_g = 9.80665;
+   //! square root of gravity constant
+   static t_real constexpr m_gSqrt = 3.131557121;
+
+    /**
+     * Computes the wave speeds.
+     *
+     * @param i_hL height of the left side.
+     * @param i_hR height of the right side.
+     * @param i_uL particle velocity of the leftside.
+     * @param i_uR particles velocity of the right side.
+     * @param o_waveSpeedL will be set to the speed of the wave propagating to the left.
+     * @param o_waveSpeedR will be set to the speed of the wave propagating to the right.
+     **/
+    static void waveSpeeds( t_real   i_hL,
+                            t_real   i_hR,
+                            t_real   i_uL,
+                            t_real   i_uR,
+                            t_real & o_waveSpeedL,
+                            t_real & o_waveSpeedR );
+
+    /**
+     * Computes the wave strengths.
+     *
+     * @param i_hL height of the left side.
+     * @param i_hR height of the right side.
+     * @param i_huL momentum of the left side.
+     * @param i_huR momentum of the right side.
+     * @param i_waveSpeedL speed of the wave propagating to the left.
+     * @param i_waveSpeedR speed of the wave propagating to the right.
+     * @param o_strengthL will be set to the strength of the wave propagating to the left.
+     * @param o_strengthR will be set to the strength of the wave propagating to the right.
+     **/
+    static void waveStrengths( t_real   i_hL,
+                               t_real   i_hR,
+                               t_real   i_huL,
+                               t_real   i_huR,
+                               t_real   i_waveSpeedL,
+                               t_real   i_waveSpeedR,
+                               t_real & o_strengthL,
+                               t_real & o_strengthR );
+
+   /**
+    * Computes the flux.
+    *
+    * @param i_h height of the left/right side.
+    * @param i_huL momentum of the left/right side.
+    * @param o_flux will be set to the flux function hu, h*u^2 + 1/2*g*h^2.
+    **/
+
+   static void flux(t_real i_h,
+                    t_real i_hu,
+                    t_real *o_flux);
 
 
-
-class fwave {
-
+public:
+   /**
+    * Computes the net-updates.
+    *
+    * @param i_hL height of the left side.
+    * @param i_hR height of the right side.
+    * @param i_huL momentum of the left side.
+    * @param i_huR momentum of the right side.
+    * @param o_netUpdateL will be set to the net-updates for the left side; 0: height, 1: momentum.
+    * @param o_netUpdateR will be set to the net-updates for the right side; 0: height, 1: momentum.
+    **/
+   static void netUpdates(t_real i_hL,
+                          t_real i_hR,
+                          t_real i_huL,
+                          t_real i_huR,
+                          t_real o_netUpdateL[2],
+                          t_real o_netUpdateR[2]);
 };
 
-
-
-#endif //TSUNAMI_LAB_FWAVE_H
+#endif
