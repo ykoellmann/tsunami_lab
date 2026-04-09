f1226a393fba197bc7b8fa0b37090cd2309831ef
mbbrueckner <mikabenbrueckner@icloud.com>
2026-04-09 20:51:04 +0200

    test: add unit test for `FWave::waveSpeeds` method

--- src/solvers/fwave/FWave.test.cpp
+++ src/solvers/fwave/FWave.test.cpp
@@ -1,5 +1,38 @@
-//
-// Created by Yannik Köllmann on 09.04.26.
-//
+/**
+* @author Yannik Köllmann (yannik.koellmann AT uni-jena.de)
+ * @author Jan Vogt (jan.vogt AT uni-jena.de)
+ * @author Mika Brückner (mika.brueckner AT uni-jena.de)
+ * @section DESCRIPTION
+ * F-wave solver for the shallow water equations.
+ **/
 
+
+#include <catch2/catch.hpp>
+#define private public
 #include "FWave.h"
+#undef public
+
+TEST_CASE( "Test the derivation of the FWave speeds.", "[FWaveSpeeds]" ) {
+  /*
+   * Test case:
+   *  h: 10 | 9
+   *  u: -3 | 3
+   *
+   * roe height: 9.5
+   * roe velocity: (sqrt(10) * -3 + 3 * 3) / ( sqrt(10) + sqrt(9) )
+   *               = -0.0790021169691720
+   * roe speeds: s1 = -0.079002116969172024 - sqrt(9.80665 * 9.5) = -9.7311093998375095
+   *             s2 = -0.079002116969172024 + sqrt(9.80665 * 9.5) =  9.5731051658991654
+   */
+  float l_waveSpeedL = 0;
+  float l_waveSpeedR = 0;
+  tsunami_lab::solvers::FWave::waveSpeeds( 10,
+                                         9,
+                                         -3,
+                                         3,
+                                         l_waveSpeedL,
+                                         l_waveSpeedR );
+
+  REQUIRE( l_waveSpeedL == Approx( -9.7311093998375095 ) );
+  REQUIRE( l_waveSpeedR == Approx(  9.5731051658991654 ) );
+}
