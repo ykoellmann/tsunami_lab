a11be3f3ce46a402e769e3ba41728639aa36bd82
Jan Vogt <jan.vogt06@web.de>
2026-04-15 13:46:15 +0200

    add submission

--- scripts/create_submission.sh
+++ scripts/create_submission.sh
@@ -28,7 +28,7 @@ if [[ $# -ne 2 ]]; then
 fi
 
 DUE_DATE="$1"
-PROJECT_PATH="$2"
+PROJECT_PATH="${2/#\~/$HOME}"
 
 if [[ ! "$DUE_DATE" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]]; then
     echo "Error: due_date must be in the format YYYY-MM-DD (e.g. 2025-10-20)."
@@ -40,7 +40,11 @@ if [[ ! -d "$PROJECT_PATH" ]]; then
     exit 1
 fi
 
-DATE_FORMATTED="$(date -d "$DUE_DATE" +%y_%m_%d)"
+if date --version &>/dev/null 2>&1; then
+    DATE_FORMATTED="$(date -d "$DUE_DATE" +%y_%m_%d)"
+else
+    DATE_FORMATTED="$(date -j -f "%Y-%m-%d" "$DUE_DATE" +%y_%m_%d)"
+fi
 DIR_NAME="submission_${DATE_FORMATTED}_brueckner_koellmann_vogt"
 TARBALL="${DIR_NAME}.tar.xz"
 
