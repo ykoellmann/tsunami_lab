1a512f6e61fb80811d50d0f228f1db0496a579b0
mbbrueckner <mikabenbrueckner@icloud.com>
2026-04-17 17:44:09 +0200

    test: add unit test for 1D wave propagation using FWave solver

--- src/patches/WavePropagation1d.test.cpp
+++ src/patches/WavePropagation1d.test.cpp
@@ -70,6 +70,62 @@ TEST_CASE("Test the 1d wave propagation solver with Roe solver.",
     REQUIRE(m_waveProp.getMomentumX()[l_ce] == Approx(0));
   }
 }
+TEST_CASE("Test the 1d wave propagation solver with f-wave solver.",
+          "[WaveProp1dFWave]") {
+  /*
+   * Test case:
+   *
+   *   Single dam break problem between cell 49 and 50.
+   *     left | right
+   *       10 | 8
+   *        0 | 0
+   *
+   *   Elsewhere steady state.
+   *
+   * The net-updates at the respective edge are given as
+   * (see derivation in FWave solver):
+   *    left                | right
+   *      9.394671362000908 | -9.394671362000908
+   *    -88.2598500         | -88.2598500
+   */
+
+  // construct solver and setup a dambreak problem
+  tsunami_lab::patches::WavePropagation1d m_waveProp(100);
+
+  for (std::size_t l_ce = 0; l_ce < 50; l_ce++) {
+    m_waveProp.setHeight(l_ce, 0, 10);
+    m_waveProp.setMomentumX(l_ce, 0, 0);
+  }
+  for (std::size_t l_ce = 50; l_ce < 100; l_ce++) {
+    m_waveProp.setHeight(l_ce, 0, 8);
+    m_waveProp.setMomentumX(l_ce, 0, 0);
+  }
+
+  // set outflow boundary condition
+  m_waveProp.setGhostOutflow();
+
+  // perform a time step
+  m_waveProp.timeStep(0.1, "FWAVE");
+
+  // steady state
+  for (std::size_t l_ce = 0; l_ce < 49; l_ce++) {
+    REQUIRE(m_waveProp.getHeight()[l_ce] == Approx(10));
+    REQUIRE(m_waveProp.getMomentumX()[l_ce] == Approx(0));
+  }
+
+  // dam-break
+  REQUIRE(m_waveProp.getHeight()[49] == Approx(10 - 0.1 * 9.394671362000908));
+  REQUIRE(m_waveProp.getMomentumX()[49] == Approx(0 + 0.1 * 88.2598500));
+
+  REQUIRE(m_waveProp.getHeight()[50] == Approx(8 + 0.1 * 9.394671362000908));
+  REQUIRE(m_waveProp.getMomentumX()[50] == Approx(0 + 0.1 * 88.2598500));
+
+  // steady state
+  for (std::size_t l_ce = 51; l_ce < 100; l_ce++) {
+    REQUIRE(m_waveProp.getHeight()[l_ce] == Approx(8));
+    REQUIRE(m_waveProp.getMomentumX()[l_ce] == Approx(0));
+  }
+}
 
 TEST_CASE("Sanity check using middle states for Roe and FWave solvers.",
           "[WaveProp1dMiddleStates]") {
