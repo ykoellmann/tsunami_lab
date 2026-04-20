4c162b5e53a898846a9c8b89f3a57790d9a26a88
ykoellmann <yankoe@outlook.de>
2026-04-20 16:09:25 +0200

    feat: add momentum left and right in DamBreak1d Setup, so examples like 2.2.2 can be calculated

--- src/setups/dambreak/DamBreak1d.cpp
+++ src/setups/dambreak/DamBreak1d.cpp
@@ -1,5 +1,5 @@
 /**
- * @author Alexander Breuer (alex.breuer AT uni-jena.de)
+* @author Alexander Breuer (alex.breuer AT uni-jena.de)
  *
  * @section DESCRIPTION
  * One-dimensional dam break problem.
@@ -7,15 +7,19 @@
 #include "DamBreak1d.h"
 
 tsunami_lab::setups::DamBreak1d::DamBreak1d(t_real i_heightLeft,
+                                            t_real i_momentumLeft,
                                             t_real i_heightRight,
+                                            t_real i_momentumRight,
                                             t_real i_locationDam) {
-  m_heightLeft = i_heightLeft;
-  m_heightRight = i_heightRight;
-  m_locationDam = i_locationDam;
+  m_heightLeft    = i_heightLeft;
+  m_momentumLeft  = i_momentumLeft;
+  m_heightRight   = i_heightRight;
+  m_momentumRight = i_momentumRight;
+  m_locationDam   = i_locationDam;
 }
 
-tsunami_lab::t_real tsunami_lab::setups::DamBreak1d::getHeight(t_real i_x,
-                                                               t_real) const {
+tsunami_lab::t_real
+tsunami_lab::setups::DamBreak1d::getHeight(t_real i_x, t_real) const {
   if (i_x < m_locationDam) {
     return m_heightLeft;
   } else {
@@ -24,8 +28,12 @@ tsunami_lab::t_real tsunami_lab::setups::DamBreak1d::getHeight(t_real i_x,
 }
 
 tsunami_lab::t_real
-tsunami_lab::setups::DamBreak1d::getMomentumX(t_real, t_real) const {
-  return 0;
+tsunami_lab::setups::DamBreak1d::getMomentumX(t_real i_x, t_real) const {
+  if (i_x < m_locationDam) {
+    return m_momentumLeft;
+  } else {
+    return m_momentumRight;
+  }
 }
 
 tsunami_lab::t_real
