e95e8931a2c9dd1b19204b30822032b71b8972fc
mbbrueckner <mbbrueckner@users.noreply.github.com>
2026-04-20 16:13:00 +0000

    deploy: a94d850826c225ae02ab84f587895d19a1ff3b56

--- doxygen/search/all_8.js
+++ doxygen/search/all_8.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['wavepropagation_0',['WavePropagation',['../classtsunami__lab_1_1patches_1_1WavePropagation.html',1,'tsunami_lab::patches']]],
   ['wavepropagation1d_1',['wavepropagation1d',['../classtsunami__lab_1_1patches_1_1WavePropagation1d.html',1,'tsunami_lab::patches::WavePropagation1d'],['../classtsunami__lab_1_1patches_1_1WavePropagation1d.html#a888e18ad0bdf59dd05c05f2dc0f90efb',1,'tsunami_lab::patches::WavePropagation1d::WavePropagation1d()']]],
-  ['write_2',['write',['../classtsunami__lab_1_1io_1_1Csv.html#a4e4243c9c33ccfa4adef35542e846113',1,'tsunami_lab::io::Csv']]]
+  ['write_2',['write',['../classtsunami__lab_1_1io_1_1Csv.html#a9ad4e144fd463c9f828bf41c8465654d',1,'tsunami_lab::io::Csv']]]
 ];
