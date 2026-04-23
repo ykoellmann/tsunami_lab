dc9000740a2ea6d066ad7734fc5444858a1ec595
mbbrueckner <mikabenbrueckner@icloud.com>
2026-04-23 21:28:38 +0200

    refactor: standardize class name to `SubCritical1d` across source files and tests

--- src/setups/subcritical1d/SubCritical1d.cpp
+++ src/setups/subcritical1d/SubCritical1d.cpp
@@ -6,13 +6,13 @@
  * @section DESCRIPTION
  * One-dimensional subcritical problem.
  **/
-#include "Subcritical1d.h"
+#include "SubCritical1d.h"
 #include <cmath>
 
-tsunami_lab::setups::Subcritical1d::Subcritical1d() {}
+tsunami_lab::setups::SubCritical1d::SubCritical1d() {}
 
 tsunami_lab::t_real
-tsunami_lab::setups::Subcritical1d::getHeight(t_real i_x, t_real) const {
+tsunami_lab::setups::SubCritical1d::getHeight(t_real i_x, t_real) const {
   if (i_x <= 25 && i_x >= 0) {
     return -getBathymetry(i_x, 0);
   } else {
@@ -21,7 +21,7 @@ tsunami_lab::setups::Subcritical1d::getHeight(t_real i_x, t_real) const {
 }
 
 tsunami_lab::t_real
-tsunami_lab::setups::Subcritical1d::getMomentumX(t_real i_x, t_real) const {
+tsunami_lab::setups::SubCritical1d::getMomentumX(t_real i_x, t_real) const {
   if (i_x <= 25 && i_x >= 0) {
     return 4.42;
   } else {
@@ -30,12 +30,12 @@ tsunami_lab::setups::Subcritical1d::getMomentumX(t_real i_x, t_real) const {
 }
 
 tsunami_lab::t_real
-tsunami_lab::setups::Subcritical1d::getMomentumY(t_real, t_real) const {
+tsunami_lab::setups::SubCritical1d::getMomentumY(t_real, t_real) const {
   return 0;
 }
 
 tsunami_lab::t_real
-tsunami_lab::setups::Subcritical1d::getBathymetry(t_real i_x, t_real) const {
+tsunami_lab::setups::SubCritical1d::getBathymetry(t_real i_x, t_real) const {
   if (i_x <= 12 && i_x >= 8) {
     return -1.8 - 0.05 * pow((i_x - 10), 2);
   } else {
