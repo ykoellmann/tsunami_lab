0395c6fc9d34f853da2fb74e207454fc2e3cc641
ykoellmann <yankoe@outlook.de>
2026-04-25 12:36:01 +0200

    feat: change to 10m in TsunamiEvent1d.cpp displacement. Also add documentation and example gifs for the tsunami

--- src/setups/tsunamievent1d/TsunamiEvent1d.cpp
+++ src/setups/tsunamievent1d/TsunamiEvent1d.cpp
@@ -58,7 +58,7 @@ tsunami_lab::setups::TsunamiEvent1d::getBathymetryRaw(t_real i_x) const {
 tsunami_lab::t_real
 tsunami_lab::setups::TsunamiEvent1d::getDisplacement(t_real i_x) const {
   if (i_x > 175000 && i_x < 250000) {
-    return 1000 * std::sin((i_x - 175000.0) / 37500.0 * M_PI + M_PI);
+    return 10 * std::sin((i_x - 175000.0) / 37500.0 * M_PI + M_PI);
   }
   return 0;
 }
