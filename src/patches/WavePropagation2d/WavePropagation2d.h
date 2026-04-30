4b89b737303ec9136990c456fe3ce368418f60d6
mbbrueckner <mikabenbrueckner@icloud.com>
2026-04-30 10:43:42 +0200

    feat: add initial files for WavePropagation2d setup

---
+++ src/patches/WavePropagation2d/WavePropagation2d.h
@@ -0,0 +1,13 @@
+//
+// Created by Mika Brückner on 30.04.26.
+//
+
+#ifndef TSUNAMI_LAB_WAVEPROPAGATION2D_H
+#define TSUNAMI_LAB_WAVEPROPAGATION2D_H
+
+
+class WavePropagation2d {
+};
+
+
+#endif //TSUNAMI_LAB_WAVEPROPAGATION2D_H
