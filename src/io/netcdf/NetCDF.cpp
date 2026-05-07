7299e0294af62b298f719e76648a108a16eefa52
mbbrueckner <mikabenbrueckner@icloud.com>
2026-05-07 10:45:43 +0200

    feat: add NetCDF class skeleton

---
+++ src/io/netcdf/NetCDF.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Mika Brückner on 07.05.26.
+//
+
+#include "NetCDF.h"
+
+namespace tsunami_lab {
+  namespace io {
+  } // io
+} // tsunami_lab
---
+++ src/io/netcdf/NetCDF.h
@@ -0,0 +1,15 @@
+//
+// Created by Mika Brückner on 07.05.26.
+//
+
+#ifndef TSUNAMI_LAB_NETCDF_H
+#define TSUNAMI_LAB_NETCDF_H
+
+namespace tsunami_lab {
+  namespace io {
+    class NetCDF {
+    };
+  } // io
+} // tsunami_lab
+
+#endif //TSUNAMI_LAB_NETCDF_H
