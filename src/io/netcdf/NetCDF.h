a359019832049221b624b0fb11c457f691b44105
mbbrueckner <mikabenbrueckner@icloud.com>
2026-05-07 10:50:41 +0200

    docs: update NetCDF class header and source with authorship and description

--- src/io/netcdf/NetCDF.h
+++ src/io/netcdf/NetCDF.h
@@ -1,15 +1,17 @@
-//
-// Created by Mika Brückner on 07.05.26.
-//
-
+/**
+ * @author Yannik Köllmann
+ * @author Jan Vogt
+ * @author Mika Brückner
+ * @section DESCRIPTION
+ * IO-routines for writing simulation data as NetCDF.
+ **/
 #ifndef TSUNAMI_LAB_NETCDF_H
 #define TSUNAMI_LAB_NETCDF_H
 
 namespace tsunami_lab {
-  namespace io {
-    class NetCDF {
-    };
-  } // io
-} // tsunami_lab
+namespace io {
+class NetCDF {};
+} // namespace io
+} // namespace tsunami_lab
 
-#endif //TSUNAMI_LAB_NETCDF_H
+#endif // TSUNAMI_LAB_NETCDF_H
