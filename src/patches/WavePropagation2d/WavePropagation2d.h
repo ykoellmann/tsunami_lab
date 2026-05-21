63eaba32ff0984d276422fccfc22984d6b3c523e
koellman <yankoe@outlook.de>
2026-05-21 12:14:15 +0200

    fix  error

--- src/patches/WavePropagation2d/WavePropagation2d.h
+++ src/patches/WavePropagation2d/WavePropagation2d.h
@@ -119,7 +119,7 @@ public:
    * @return water heights.
    */
   t_real const* getHeight() {
-    return m_h + m_step * (m_nCells_x + 2) * getStride() + getStride() + 1;
+    return m_h + m_step * (m_nCells_x + 2) * (m_nCells_y + 2) + getStride() + 1;
   }
 
   /**
@@ -128,7 +128,7 @@ public:
    * @return momenta in x-direction.
    **/
   t_real const* getMomentumX() {
-    return m_hu + m_step * (m_nCells_x + 2) * getStride() + getStride() + 1;
+    return m_hu + m_step * (m_nCells_x + 2) * (m_nCells_y + 2) + getStride() + 1;
   }
 
   /**
@@ -137,7 +137,7 @@ public:
    * @return momenta in y-direction.
    **/
   t_real const* getMomentumY() {
-    return m_hv + m_step * (m_nCells_x + 2) * getStride() + getStride() + 1;
+    return m_hv + m_step * (m_nCells_x + 2) * (m_nCells_y + 2) + getStride() + 1;
   }
 
   /**
